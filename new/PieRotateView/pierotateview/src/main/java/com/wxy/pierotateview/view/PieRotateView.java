3b36316c62e98cab2dd0a1e1fc4f58d396f85a7a
NoEndToLF <871601607@qq.com>
2019-12-08 15:29:48 +0800

    优化

--- new/PieRotateView/pierotateview/src/main/java/com/wxy/pierotateview/view/PieRotateView.java
+++ new/PieRotateView/pierotateview/src/main/java/com/wxy/pierotateview/view/PieRotateView.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.wxy.pierotateview.R;
@@ -20,7 +23,8 @@ public class PieRotateView extends View {
     private Paint piePaint,circlePaint,textPaint;
     protected static final int DEFUALT_VIEW_WIDTH=400;
     protected static final int DEFUALT_VIEW_HEIGHT=400;
-    private int radius,centerX,centerY;
+    private float radius,centerX,centerY;
+    private float downX,downY,lastX,lastY;
     private float sum;
     public void setCircleColor(int circleColor) {
         circlePaint.setColor(circleColor);
@@ -53,7 +57,7 @@ public class PieRotateView extends View {
         piePaint.setColor(Color.BLUE);
         circlePaint=new Paint();
         circlePaint.setAntiAlias(true);
-        circlePaint.setColor(Color.parseColor("#45ffffff"));
+        circlePaint.setColor(Color.parseColor("#75ffffff"));
         textPaint=new Paint();
         textPaint.setAntiAlias(true);
         textPaint.setTextAlign(Paint.Align.CENTER);
@@ -104,10 +108,39 @@ public class PieRotateView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         if (radius==0){
-            radius= (int) ((Math.min(getWidth()-getPaddingLeft()-getPaddingRight(),getHeight())-getPaddingTop()-getPaddingBottom())/2);
+            radius= (Math.min(getWidth()-getPaddingLeft()-getPaddingRight(),getHeight())-getPaddingTop()-getPaddingBottom())/2;
                 centerX=getPaddingLeft()+(getWidth()-getPaddingLeft()-getPaddingRight())/2;
                 centerY=getPaddingTop()+(getHeight()-getPaddingTop()-getPaddingBottom())/2;
         }
+        //先画所有扇形
+        drawDataArc(canvas);
+        //画中间的圆形
+        canvas.drawCircle(centerX,centerY,radius/2.4f,circlePaint);
+        //画Text
+        textPaint.setTextSize(radius/4.2f);
+        textPaint.setColor(Color.WHITE);
+        canvas.drawText("20%",centerX,centerY-getTextOffset(textPaint,"20%"),textPaint);
+        //画指针
+        float arrowRadius=radius/4f;
+        canvas.save();
+        Path area=new Path();
+        area.addCircle(centerX,centerY,radius, Path.Direction.CCW);
+        canvas.clipPath(area);
+        Path arrowPath=new Path();
+        arrowPath.moveTo(centerX,centerY+radius-arrowRadius);
+        arrowPath.lineTo(centerX-arrowRadius/2,centerY+radius);
+        arrowPath.lineTo(centerX+arrowRadius/2,centerY+radius);
+        arrowPath.close();
+        canvas.drawPath(arrowPath,circlePaint);
+        canvas.restore();
+    }
+    public float getTextOffset(Paint paint, String text){
+        Rect bounds=new Rect();
+        paint.getTextBounds(text,0,text.length(),bounds);
+        float offset=(bounds.top+bounds.bottom)/2;
+        return offset;
+    }
+    private void drawDataArc(Canvas canvas) {
         if (pieRotateViewModelList!=null&&pieRotateViewModelList.size()>0){
             float hasDregee=0;
             for (int i=0;i<pieRotateViewModelList.size();i++){
@@ -115,9 +148,9 @@ public class PieRotateView extends View {
                 switch (i){
                     case 0:
                         hasDregee=90f-pieRotateViewModelList.get(i).getNum()/sum*360f/2f;
-                         break;
-                        default:
-                            hasDregee=hasDregee+  pieRotateViewModelList.get(i-1).getNum()/sum*360f  ;
+                        break;
+                    default:
+                        hasDregee=hasDregee+  pieRotateViewModelList.get(i-1).getNum()/sum*360f  ;
                 }
                 canvas.drawArc(new RectF(centerX-radius, centerY-radius,
                                 centerX+radius, centerY+radius),
@@ -125,5 +158,52 @@ public class PieRotateView extends View {
 
             }
         }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return true;
+    }
+    /**获得相对于X轴正方向的夹角*/
+
+    public float degree(float x, float y){
+        float detaX = x - centerX;
+        float detaY = y - centerY;
+        float degree=0;
+        if (detaX==0&&detaY!=0){
+            //在Y轴上
+            if (detaY>0){
+                degree=90;
+            }else{
+                degree=270;
+            }
+        }else if(detaY==0&&detaX!=0){
+            //在X轴上
+            if (detaX>0){
+                degree=0;
+            }else{
+                degree=180;
+            }
+        }else if (detaX>0&&detaY>0){
+            //第一象限内
+            float tan = Math.abs(detaY / detaX);
+            degree=(float)Math.toDegrees(Math.atan(tan));
+        }else if (detaX<0&&detaY>0){
+            //第二象限内
+            float tan = Math.abs(detaX / detaY);
+            degree=90+(float)Math.toDegrees(Math.atan(tan));
+        }else if (detaX<0&&detaY<0){
+            //第三象限内
+            float tan = Math.abs(detaY / detaX);
+            degree=180+(float)Math.toDegrees(Math.atan(tan));
+        }else if (detaX>0&&detaY<0){
+            //第四象限内
+            float tan = Math.abs(detaX / detaY);
+            degree=270+(float)Math.toDegrees(Math.atan(tan));
+        }else{
+            //在原点位置
+            degree=0;
+        }
+        return degree;
     }
 }
